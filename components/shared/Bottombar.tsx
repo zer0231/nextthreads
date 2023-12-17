e7f3d360993c07566656512acb625a3dd91e2e1e
zer0231 <pranay_zer0@protonmail.com>
2023-12-17 22:32:23 +0545

    Finished bottombar and made it responsive

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+"use client"
 
+import React from 'react'
+import { sidebarLinks } from '@/constants/index'
+import Link from 'next/link'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation';
 export default function Bottombar() {
+    const pathName = usePathname(); //A hook that contains list of active routes
   return (
 <section className="bottombar  md:hidden">
     <div className="bottombar_container">
-        {}
+    {
+                    sidebarLinks.map((link) => {
+                        const isActive = (pathName.includes(link.route) && link.route.length > 1) || pathName === link.route
+                        return (
+                      
+                                <Link href={link.route} key={link.label} className={`bottombar_link ${isActive && 'bg-primary-500'}`}>
+                                    <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+                                    <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                                        {link.label.split(/\s+/)[0]} {/* splits the text by space and get the first letter only */}
+                                    </p>
+                                </Link>
+                        
+                        )
+                    })
+
+                }
     </div>
 </section>
   )
